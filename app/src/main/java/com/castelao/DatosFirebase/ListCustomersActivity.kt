b0e1d46799b5f7d68cb7d874dd55e5d40813d8c1
arh91 <alvaro_rh_991@hotmail.com>
2023-12-28 13:15:04 +0100

    En los layouts para listar clientes o proveedores, introduzco un nuevo botón cuya función será la de listar únicamente el registro cuyo nombre coincida con el del introducido por el usuario en el campo de texto.

--- app/src/main/java/com/castelao/DatosFirebase/ListCustomersActivity.kt
+++ app/src/main/java/com/castelao/DatosFirebase/ListCustomersActivity.kt
@@ -3,7 +3,10 @@ package com.castelao.DatosFirebase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,12 +16,24 @@ class ListCustomersActivity : AppCompatActivity(), CustomersAdapter.OnItemClickL
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var atras: Button
+    private lateinit var textViewNombre: TextView
+    private lateinit var nombreCliente: EditText
+    private lateinit var ok: Button
+    private lateinit var buscarCliPorNombre: Button
     private lateinit var firebaseDatabase: FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_customers)
+
+        textViewNombre = findViewById(R.id.textView_nombreCli)
+        nombreCliente = findViewById(R.id.editTextNombreCli)
+        ok = findViewById(R.id.btnOkCli)
+
+        textViewNombre.visibility = View.GONE
+        nombreCliente.visibility = View.GONE
+        ok.visibility = View.GONE
 
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -27,6 +42,27 @@ class ListCustomersActivity : AppCompatActivity(), CustomersAdapter.OnItemClickL
         atras.setOnClickListener(){
             val toFifth = Intent(this, FifthActivity::class.java)
             startActivity(toFifth)
+        }
+
+        buscarCliPorNombre = findViewById(R.id.findSupplierByName)
+        buscarCliPorNombre.setOnClickListener(){
+            recyclerView.visibility = View.GONE
+            atras.visibility = View.GONE
+            buscarCliPorNombre.visibility = View.GONE
+            textViewNombre.visibility = View.VISIBLE
+            nombreCliente.visibility = View.VISIBLE
+            ok.visibility = View.VISIBLE
+        }
+
+        ok.setOnClickListener(){
+            val nombre = nombreCliente.text.toString()
+
+            textViewNombre.visibility = View.GONE
+            nombreCliente.visibility = View.GONE
+            ok.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+            atras.visibility = View.VISIBLE
+            buscarCliPorNombre.visibility = View.VISIBLE
         }
 
         firebaseDatabase = FirebaseDatabase.getInstance()
@@ -50,6 +86,31 @@ class ListCustomersActivity : AppCompatActivity(), CustomersAdapter.OnItemClickL
 
                 val adapter = CustomersAdapter(dataList, this@ListCustomersActivity)
                 recyclerView.adapter = adapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Si los datos no se han podido mostrar correctamente, lanzamos aviso al usuario
+                Toast.makeText(this@ListCustomersActivity, "No se pudieron obtener los datos. $error", Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+    }
+
+    private fun listarCliente(nombre: String){
+        val query = databaseReference.orderByChild("nombre").equalTo(nombre)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val dataList = mutableListOf<String>()
+
+                for (dataSnapshot in snapshot.children) {
+                    val provider = dataSnapshot.child("codigo").getValue(String::class.java)
+                    provider?.let {
+                        dataList.add(it)
+                    }
+                }
+                val adapter = CustomersAdapter(dataList, this@ListCustomersActivity)
+                recyclerView.adapter = adapter
+                //providersAdapter.notifyDataSetChanged()
             }
 
             override fun onCancelled(error: DatabaseError) {
--- app/src/main/java/com/castelao/DatosFirebase/ListSuppliersActivity.kt
+++ app/src/main/java/com/castelao/DatosFirebase/ListSuppliersActivity.kt
@@ -3,7 +3,10 @@ package com.castelao.DatosFirebase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,12 +16,24 @@ class ListSuppliersActivity : AppCompatActivity(), SuppliersAdapter.OnItemClickL
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var atras: Button
+    private lateinit var textViewNombre: TextView
+    private lateinit var nombreProveedor: EditText
+    private lateinit var ok: Button
+    private lateinit var buscarProvPorNombre: Button
     private lateinit var firebaseDatabase: FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_suppliers)
+
+        textViewNombre = findViewById(R.id.textView_nombreProv)
+        nombreProveedor = findViewById(R.id.editTextNombreProv)
+        ok = findViewById(R.id.btnOkProv)
+
+        textViewNombre.visibility = View.GONE
+        nombreProveedor.visibility = View.GONE
+        ok.visibility = View.GONE
 
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -27,6 +42,27 @@ class ListSuppliersActivity : AppCompatActivity(), SuppliersAdapter.OnItemClickL
         atras.setOnClickListener(){
             val toFourth = Intent(this, FourthActivity::class.java)
             startActivity(toFourth)
+        }
+
+        buscarProvPorNombre = findViewById(R.id.findSupplierByName)
+        buscarProvPorNombre.setOnClickListener(){
+            recyclerView.visibility = View.GONE
+            atras.visibility = View.GONE
+            buscarProvPorNombre.visibility = View.GONE
+            textViewNombre.visibility = View.VISIBLE
+            nombreProveedor.visibility = View.VISIBLE
+            ok.visibility = View.VISIBLE
+        }
+
+        ok.setOnClickListener(){
+            val nombre = nombreProveedor.text.toString()
+
+            textViewNombre.visibility = View.GONE
+            nombreProveedor.visibility = View.GONE
+            ok.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+            atras.visibility = View.VISIBLE
+            buscarProvPorNombre.visibility = View.VISIBLE
         }
 
         firebaseDatabase = FirebaseDatabase.getInstance()
@@ -59,6 +95,32 @@ class ListSuppliersActivity : AppCompatActivity(), SuppliersAdapter.OnItemClickL
             }
         })
     }
+
+    private fun listarProveedor(nombre: String){
+        val query = databaseReference.orderByChild("nombre").equalTo(nombre)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val dataList = mutableListOf<String>()
+
+                for (dataSnapshot in snapshot.children) {
+                    val provider = dataSnapshot.child("codigo").getValue(String::class.java)
+                    provider?.let {
+                        dataList.add(it)
+                    }
+                }
+                val adapter = SuppliersAdapter(dataList, this@ListSuppliersActivity)
+                recyclerView.adapter = adapter
+                //providersAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Si los datos no se han podido mostrar correctamente, lanzamos aviso al usuario
+                Toast.makeText(this@ListSuppliersActivity, "No se pudieron obtener los datos. $error", Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+    }
+
 
     override fun accederDatosProveedor(code: String) {
         // Al hacer clic en un elemento, abrir la actividad de detalles
