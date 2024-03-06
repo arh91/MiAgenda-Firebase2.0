1b2a3c4f2b3ba36c9df2daf39ec9907bd4d9acca
arh91 <alvaro_rh_991@hotmail.com>
2024-03-07 00:25:34 +0100

    Programo desde el código kotlin las dimensiones de ancho y alto de las vistas de activity_main para orientación landscape.

--- app/src/main/java/com/castelao/DatosFirebase/MainActivity.kt
+++ app/src/main/java/com/castelao/DatosFirebase/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.castelao.DatosFirebase
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 
 class
@@ -21,21 +22,23 @@ MainActivity : AppCompatActivity() {
 
         val registro = findViewById<Button>(R.id.btnRegistro)
         val salir = findViewById<Button>(R.id.btnSalir)
+
         /*val gosecond = findViewById<Button>(R.id.askMeBtn)
         gosecond.setOnClickListener{
             // Crea un Intent para iniciar la segunda actividad
 
 
 
-// Añade datos adicionales al Intent
-            intent.putExtra("proveedor", "Castelao")
-            intent.putExtra("cliente", "Google")
 
-// Inicia la segunda actividad
-            startActivityForResult(intent, RESULTADO_UNO)
+        // Añade datos adicionales al Intent
+                    intent.putExtra("proveedor", "Castelao")
+                    intent.putExtra("cliente", "Google")
 
-            startActivityForResult(intent, RESULTADO_DOS)
-        }*/
+        // Inicia la segunda actividad
+                    startActivityForResult(intent, RESULTADO_UNO)
+
+                    startActivityForResult(intent, RESULTADO_DOS)
+                }*/
 
 
         registro.setOnClickListener{
@@ -46,11 +49,52 @@ MainActivity : AppCompatActivity() {
         salir.setOnClickListener{
             finishAffinity()
         }
+    }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            asignarDimensionesVistas()
+            Toast.makeText(this@MainActivity, "LANDSCAPE!", Toast.LENGTH_SHORT).show()
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(this@MainActivity, "PORTRAIT!", Toast.LENGTH_SHORT).show()
+        }
+    }
 
+    fun asignarDimensionesVistas(){
+        val displayMetrics = resources.displayMetrics
+        val screenWidth = displayMetrics.widthPixels
+        val screenHeight = displayMetrics.heightPixels
 
+        // Calcula las dimensiones basadas en porcentajes
+        val widthPercentageTitle = 0.336 // Porcentaje del 33,6%
+        val heightPercentageTitle = 0.145 // Porcentaja del 14,5%
+        val widthPercentageButton = 0.19 // Porcentaje del 19%
+        val heightPercentageButton = 0.145 // Porcentaje del 14,5
+        // %
 
+        val widthTitle = (screenHeight * widthPercentageTitle).toInt()
+        val heightTitle = (screenWidth * heightPercentageTitle).toInt()
+        val widthButton = (screenHeight * widthPercentageButton).toInt()
+        val heightButton = (screenWidth * heightPercentageButton).toInt()
 
+        val title = findViewById<TextView>(R.id.tituloLand)
+        val tituloParams = title.layoutParams
+        tituloParams.width = widthTitle
+        tituloParams.height = heightTitle
+        title.layoutParams = tituloParams
+
+        val btnOne = findViewById<Button>(R.id.btnRegistroLand)
+        val btnRegistroParams = btnOne.layoutParams
+        btnRegistroParams.width = widthButton // Asigna el valor calculado a la anchura de btnRegistroLand en el layout
+        btnRegistroParams.height = heightButton // Asigna el valor calculado a la altura de btnRegistroLand en el layout
+        btnOne.layoutParams = btnRegistroParams
+
+        val btnTwo = findViewById<Button>(R.id.btnSalirLand)
+        val btnSalirParams = btnTwo.layoutParams
+        btnSalirParams.width = widthButton
+        btnSalirParams.height = heightButton
+        btnTwo.layoutParams = btnSalirParams
     }
 
     /*@Deprecated("Deprecated in Java")
